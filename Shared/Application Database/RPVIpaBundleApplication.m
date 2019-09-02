ae383519f65f4f94976a0e43bc6656167ce2d694
Matt Clarke <mclarke0805@gmail.com>
2019-09-02 17:40:24 +0100

    Use the display name of an incoming IPA

--- Shared/Application Database/RPVIpaBundleApplication.m
+++ Shared/Application Database/RPVIpaBundleApplication.m
@@ -278,7 +278,7 @@
     return [self.cachedInfoPlist objectForKey:@"CFBundleIdentifier"];
 }
 - (NSString*)applicationName {
-    return [self.cachedInfoPlist objectForKey:@"CFBundleName"];
+    return [self.cachedInfoPlist objectForKey:@"CFBundleDisplayName"];
 }
 
 - (NSString*)applicationVersion {
